9debf9aaa2a8344e2645ca5a32bb27842ef460a1
194912 <194912@CARBON.CTI.P.LODZ.PL>
2018-03-25 11:57:18 +0200

    [Task 3] Added email endpoint to server

--- lab3/server.js
+++ lab3/server.js
@@ -7,6 +7,15 @@ const PORT = 8080;
 
 app.use(bodyParser.json());
 
+app.post('/api/email', (req, res) => {
+	var body = req.body;
+
+	
+	
+	res.setHeader('content-type', 'application/json');
+	res.end(JSON.stringify(req.body));
+});
+
 app.listen(PORT, () => {
 	console.log('Server is listening on', PORT);
 });
